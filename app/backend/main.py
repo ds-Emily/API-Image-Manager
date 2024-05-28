2e9abab9fded7c44c6e044f823b1305fa74df4e9
ds-Emily <ds.emilypenaranda@gmail.com>
2024-05-28 15:50:31 +0200

    solving problems

--- app/backend/main.py
+++ app/backend/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, , render_template
+from flask import Flask, jsonify, render_template
 from http_requests.image_api import ImageAPI
 from services.image_service import ImageProcessor
 
